ab1fbd7e468bd04581a6b463a19f00f9564aff64
Lucem <lucem@Lucems-MacBook-Air.local>
2024-02-11 11:15:41 +0300

    UI Update Transactions

--- src/components/Transaction.svelte
+++ src/components/Transaction.svelte
@@ -5,18 +5,18 @@
     export let transaction;
 </script>
 
-<div class="card row bg-fade mr-4 mb-4 w-full">
+<div class="row mr-4 mb-4 w-full hover:cursor-pointer" on:click={() => emit('create', transaction)} on:keydown>
     <div> <div class="w-2 h-full rounded-l-xl" style="background-color: {transaction.color}"></div> </div>
     <div class="py-1 px-4 border-y border-primary flex-1">
         <h3 class="text-sm font-bold">{transaction.name}</h3>
         <p class="text-xs">{transaction.time.toLocaleString()}</p>
     </div>
-    <div class="row border-y border-r border-primary rounded-r-xl items-center">
-        <p class="text-xs"><span class="{transaction.amount>=0? 'text-green-500': 'text-red-500'} font-bold text-lg">{transaction.amount}</span> {transaction.currency}</p>
-        <button on:click={()=>emit('create', transaction)}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-            </svg>
-        </button>
+    <div class="flex border-y border-r border-primary rounded-r-xl pr-4">
+        <p class="text-xs text-end my-auto">
+            <span class="text-xs">{transaction.currency}</span>
+            <br>
+            <span>{transaction.fee || 0}</span>
+        </p>
+        <p class="{transaction.amount>=0? 'text-green-500': 'text-red-500'} font-bold text-2xl">{transaction.amount}</p>
     </div>
 </div>
